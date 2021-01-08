8ed376fead860944d765f82138608191afd3f7d1
Zaid Ajaz <zaidajaz@Zaids-MacBook-Air.local>
2021-01-08 22:33:46 +0530

    Fixed the phone number bug

--- src/components/user-cards/user-card.js
+++ src/components/user-cards/user-card.js
@@ -7,7 +7,7 @@ function UserCard(props) {
             <img className="cmp-user-card__avatar" src={`https://i.pravatar.cc/300?img=${props.user.id + 13}`}></img>
             <div className="cmp-user-card__details">
                 <h2 className="cmp-user-card__heading">{props.user.name} ({props.user.username})</h2>
-                <div className="cmp-user-card__sub-heading">{props.user.email} | {props.user.email} | {props.user.website}</div>
+                <div className="cmp-user-card__sub-heading">{props.user.phone} | {props.user.email} | {props.user.website}</div>
                 <ImLocation /> <span>{props.user.address.street}, {props.user.address.city} - {props.user.address.zipcode}</span>
                 {/* <button className="cmp-user-card__action">View Posts</button> */}
                 <Link className="cmp-user-card__action" to={`/posts/${props.user.id}`}>View Posts</Link>
